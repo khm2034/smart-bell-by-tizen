a093c98d284e9ea7d42e56e706e4b88d8054fb87
Seongju Hwang <ajanab153@naver.com>
2018-02-27 20:36:40 +0900

    Add files via upload
    add function about setting password using matrix keypad [ver 1.0]

--- position-finder-server/src/controller.c
+++ position-finder-server/src/controller.c
@@ -38,6 +38,13 @@
 #define TEST_CAMERA_SAVE 0
 #define CAMERA_ENABLED 0
 
+char init_pw[8];
+char input_pw[8];
+int init_index = 0;
+int input_index = 0;
+int error_count=0;
+int state = 0;
+
 typedef struct app_data_s {
 	Ecore_Timer *getter_timer;
 	connectivity_resource_s *resource_info;
@@ -45,7 +52,7 @@ typedef struct app_data_s {
 
 static void __resource_camera_capture_completed_cb(const void *image, unsigned int size, void *user_data)
 {
-	/* TODO */
+
 	const char *path = NULL;
 	const char *url = NULL;
 
@@ -109,14 +116,22 @@ static void _ultrasonic_sensor_read_cb(double value, void *data)
 	return;
 }
 static int cnt = 0;
+
 static Eina_Bool control_sensors_cb(void *data)
 {
 	app_data *ad = data;
-	//int value = 1;
-	int ret = 0;
+	uint32_t value = -1;
+	int ret = -1;
 
-	//_D("Detected distance : %d", dist);
 #if 0
+	ret = resource_read_ultrasonic_sensor(20, 21, _ultrasonic_sensor_read_cb, NULL);
+	if (ret != 0) _E("Cannot read sensor value");
+
+	_D("Detected distance : %lf", dist);
+#endif
+
+
+#if CAMERA_ENABLED
 	static unsigned int count = 0;
 
 	if (count % CAMERA_TIME_INTERVAL == 0) {
@@ -128,12 +143,6 @@ static Eina_Bool control_sensors_cb(void *data)
 	count++;
 #endif
 
-#if 0
-	ret = resource_read_ultrasonic_sensor(20, 21, _ultrasonic_sensor_read_cb, NULL);
-	if (ret != 0) _E("Cannot read sensor value");
-
-	_D("Detected distance : %lf", dist);
-#endif
 #if 0 // servo motor
 	// GND, OE : ground , SCR : 5pin, SDA : 3pin,  vcc v+ : 5v
 	_D("cycle : %d", cycle);
@@ -178,12 +187,202 @@ static Eina_Bool control_sensors_cb(void *data)
 //	//_D("Detected distance : %lf", dist);
 //#endif
 
+#if 0 //led
+    //ret = resource_write_led(21,1);
+    //_D("Turn on the led");
+      ret = resource_write_led(4,1);
+    _D("Turn off the led");
+    if(ret!=0)
+    {
+        _E("Cannot read led");
+    }
+#endif
+
+#if 0 //buzzer
+      ret = resource_write_buzzer(4,1);
+    _D("Turn off the buzzer");
+    if(ret!=0)
+    {
+        _E("Cannot read buzzer");
+    }
+#endif
+
+#if 0 //touch
+    resource_write_led(21,0);
+    resource_write_buzzer(4,0);
+    ret = resource_read_touch_sensor(17 , &value);
+    ret2 = resource_read_touch_sensor(24 , &value2);
+    /*if(ret != 0 || ret2 != 0)
+        {
+            _E("Cannot read flame");
+            resource_write_led(8,0);
+        }*/
+    if (value == 0){
+    	_D("Turn on the led by touching");
+    	resource_write_led(21,1);
+    }
+    else if(value2 == 0)
+    {
+    	resource_write_buzzer(4,1);
+    }
+    else{
+        _D("Turn off the led by no touching");
+        resource_write_led(21,0);
+        resource_write_buzzer(4,0);
+    }
+#endif
+
+#if 0 //flame
+    _D("reading flame sensor value");
+    ret = resource_read_flame_sensor(20, &value);
+    if(ret != 0)
+    {
+        _E("Cannot read flame");
+        //ret = resource_write_led(21,0);
+    }
+    else{
+        _D("Turn on the led");
+        ret = resource_write_led(21,1);
+        _D("Turn off the led");
+        ret = resource_write_led(21,0);
+
+
+        /*if(position < .5)
+        {
+            _D("Turn on the led");
+            ret = resource_write_led(21,1);
+            return ECORE_CALLBACK_RENEW;
+        }
+        _D("Turn off the led");
+        ret = resource_write_led(21,0);
+        return ECORE_CALLBACK_CANCEL;
+    }
+    ecore_animator_timeline_add(5.,_control_sensors_cb,0);*/
+#endif
+
+#if 0 //vibe
+    resource_read_vibration_sensor(20, &value);
+    if(value == 0)
+    {
+        _E("no vibe");
+        _D("Turn off the led");
+        ret = resource_write_led(21,0);
+        if(connectivity_notify_double(ad->resource_info,"distance",value)==-1)
+                    _E("Cannot notify message");
+    }
+    else
+    {
+        _D("vibe");
+        _D("Turn on the led");
+        ret = resource_write_led(21,1);
+        if(connectivity_notify_double(ad->resource_info,"vibe",value)==-1)
+                    _E("Cannot notify message");
+    }
+
+#endif
+
 #if 1
 	char getch=NULL;
+	resource_write_led(4,0);
+	resource_write_buzzer(24,0);
 	//_D("read key matrix");
 	resource_read_key_matrix(&getch);
-	if(getch != NULL)
-		_D("read [%c]key", getch);
+	if(getch != NULL){
+		switch(getch)
+		{
+		case 'A':
+			if(init_pw[0]!=NULL){
+				_D(" [Set entering value as password] ");
+				state = 1;
+				resource_write_led(4,1);
+			}
+			else
+			{
+				_E("Must enter Init password");
+			}
+			break;
+		case 'B':
+			if(state==0)
+			{
+				if(init_index>0){
+					_D("[Delete] previous initial value ");
+					init_pw[init_index-1]=NULL;
+					init_index--;
+				}
+				else {
+					_E("This command can't be executed");
+				}
+			}
+			else if(state==1)
+			{
+				if(input_index>0)
+				{
+					_D("[Delete] previous input value");
+					input_pw[input_index-1]=NULL;
+					input_index--;
+				}
+				else
+				{
+					_E("This command can't be executed");
+				}
+			}
+			break;
+		case 'C':
+			clear:
+			if(state==0)
+			{
+				_D("[Reset] Initial password is entering reset");
+				init_pw[0]='\0';
+				init_index=0;
+			}
+			else if(state==1)
+			{
+				_D("[Reset] Input password is entering reset");
+				input_pw[0]='\0';
+				input_index=0;
+			}
+			break;
+		case 'D':
+			_D("[ Display Now values]");
+			_D("[ Now init value : %s ]",init_pw);
+			_D("[ Now input value : %s ]",input_pw);
+			break;
+		case '*':
+			if(strcmp(init_pw,input_pw)==0)
+			{
+				_D("[Success] Open");
+				resource_write_led(4,1);
+			}
+			else if(strcmp(init_pw,input_pw)!=0)
+			{
+				_E("[Error] please retry");
+				error_count++;
+				if(error_count>=3)
+				{
+					_E("[Fail] Exceed input count");
+					resource_write_buzzer(24,1);
+				}
+				else
+				{
+					goto clear;
+				}
+			}
+			break;
+		default:
+			if(state==0)
+			{
+				_D("Now input [%c]key", getch);
+				init_pw[init_index++]=getch;
+			}
+			else if(state==1)
+			{
+				_D("Now input [%c]key", getch);
+				input_pw[input_index++]=getch;
+			}
+			break;
+		}
+	}
+
 #endif
 	/* This is example, get value from sensors first */
 //	if (connectivity_notify_int(ad->resource_info, "Motion", value) == -1)
@@ -207,7 +406,7 @@ static bool service_app_create(void *data)
 	/**
 	 * Create a connectivity resource and registers the resource in server.
 	 */
-	connectivity_set_protocol(CONNECTIVITY_PROTOCOL_HTTP);
+	/*connectivity_set_protocol(CONNECTIVITY_PROTOCOL_HTTP);
 
 	controller_util_get_path(&path);
 	if (path == NULL) {
@@ -215,19 +414,18 @@ static bool service_app_create(void *data)
 		return false;
 	}
 
-	ret = connectivity_set_resource(path, "org.tizen.door", &ad->resource_info);
-	if (ret == -1) _E("Cannot broadcast resource");
+	ret = connectivity_set_resource(path, "org.tizen.door", &ad->resource_info);*/
+	//if (ret == -1) _E("Cannot broadcast resource");
 
 	/**
 	 * Creates a timer to call the given function in the given period of time.
 	 * In the control_sensors_cb(), each sensor reads the measured value or writes a specific value to the sensor.
 	 */
-	//ad->getter_timer = ecore_timer_add(0.001f, control_sensors_cb, ad);
-	ad->getter_timer = ecore_timer_add(1.0f, control_sensors_cb, ad);
-	if (!ad->getter_timer) {
+	ad->getter_timer = ecore_timer_add(1.5f, control_sensors_cb, ad);
+	/*if (!ad->getter_timer) {
 		_E("Failed to add infrared motion getter timer");
 		return false;
-	}
+	}*/
 
 	return true;
 }

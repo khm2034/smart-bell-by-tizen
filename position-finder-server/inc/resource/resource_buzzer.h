1758a3c0cd50ab99ae60b21d05e295935fd29ba5
Seongju Hwang <ajanab153@naver.com>
2018-02-27 20:43:34 +0900

    add buzzer
    add buzzer

---
+++ position-finder-server/inc/resource/resource_buzzer.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Contact: Jin Yoon <jinny.yoon@samsung.com>
+ *          Geunsun Lee <gs86.lee@samsung.com>
+ *          Eunyoung Lee <ey928.lee@samsung.com>
+ *          Junkyu Han <junkyu.han@samsung.com>
+ *
+ * Licensed under the Flora License, Version 1.1 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __POSITION_FINDER_RESOURCE_BUZZER_H__
+#define __POSITION_FINDER_RESOURCE_BUZZER_H__
+
+extern int resource_write_buzzer(int pin_num, int write_value);
+
+#endif /* __POSITION_FINDER_RESOURCE_LED_H__ */
